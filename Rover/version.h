e5b61efef52605696030af5bd3e9b58f24f7ab23
Luca Pescante <luca@pescante.it>
2023-10-07 16:41:11 +0800

    add Align firmware version

--- Rover/version.h
+++ Rover/version.h
@@ -11,9 +11,16 @@
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,3,FIRMWARE_VERSION_TYPE_OFFICIAL
 
+// ArduPilot Version
 #define FW_MAJOR 4
 #define FW_MINOR 2
 #define FW_PATCH 3
 #define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+
+// Align version 
+#define MIDDLE_MAJOR 0
+#define MIDDLE_MINOR 1
+#define MIDDLE_PATCH 0
+#define MIDDLE_TYPE FIRMWARE_VERSION_TYPE_DEV
 
 #include <AP_Common/AP_FWVersionDefine.h>
