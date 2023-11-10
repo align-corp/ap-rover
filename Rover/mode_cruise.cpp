a1876274a0216f924c9ac1a580788f3e340c28b0
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2023-11-10 16:04:09 +0100

    implementing cruise mode

--- Rover/mode_cruise.cpp
+++ Rover/mode_cruise.cpp
@@ -5,6 +5,7 @@
 bool ModeCruise::_enter()
 {
     gcs().send_text(MAV_SEVERITY_CRITICAL, "Entering Cruise Mode");
+    _speed_input = 0;
     return true;
 }
 
@@ -17,36 +18,41 @@ void ModeCruise::_exit()
 
 void ModeCruise::update()
 {
-    float steering_out, throttle_out, desired_lateral;
-    // do basic conversion
-    get_pilot_input(steering_out, throttle_out);
-    gcs().send_text(MAV_SEVERITY_CRITICAL, "steering_in: %f, throttle_in %f", steering_out, throttle_out);
+    float steering, throttle, speed;
+    get_pilot_input(steering, throttle);
+    _speed_input += throttle * g.cruise_p*0.01f;
+
+    // do not reverse and stop immediately if minimum throttle
+    if (_speed_input < 0.0f || throttle < -99.0f) {
+        _speed_input = 0.0f;
+    } else if (_speed_input > 100.0f) {
+        _speed_input = 100.0f;
+    }
+    speed = _speed_input;
 
     // for skid steering vehicles, if pilot commands would lead to saturation
-    // we proportionally reduce steering and throttle
+    // we proportionally reduce steering and _speed_input
     if (g2.motors.have_skid_steering()) {
-        const float steer_normalised = constrain_float(steering_out / 4500.0f, -1.0f, 1.0f);
-        const float throttle_normalised = constrain_float(throttle_out / 100.0f, -1.0f, 1.0f);
-        const float saturation_value = fabsf(steer_normalised) + fabsf(throttle_normalised);
+        const float steer_normalised = constrain_float(steering / 4500.0f, -1.0f, 1.0f);
+        const float _speed_normalised = constrain_float(_speed_input / 100.0f, -1.0f, 1.0f);
+        const float saturation_value = fabsf(steer_normalised) + fabsf(_speed_normalised);
         if (saturation_value > 1.0f) {
-            steering_out /= saturation_value;
-            throttle_out /= saturation_value;
+            steering /= saturation_value;
+            speed = _speed_input/saturation_value;
         }
     }
     // check for special case of input and output throttle being in opposite directions
-    float throttle_out_limited = g2.motors.get_slew_limited_throttle(throttle_out, rover.G_Dt);
-    if ((is_negative(throttle_out) != is_negative(throttle_out_limited)) &&
+    float throttle_limited = g2.motors.get_slew_limited_throttle(speed, rover.G_Dt);
+    if ((is_negative(speed) != is_negative(throttle_limited)) &&
         ((g.pilot_steer_type == PILOT_STEER_TYPE_DEFAULT) ||
          (g.pilot_steer_type == PILOT_STEER_TYPE_DIR_REVERSED_WHEN_REVERSING))) {
-        steering_out *= -1;
+        steering *= -1;
     }
-    throttle_out = throttle_out_limited;
+    speed = throttle_limited;
 
-    get_pilot_desired_lateral(desired_lateral);
-    gcs().send_text(MAV_SEVERITY_CRITICAL, "steering: %f, throttle %f, lateral: %f", steering_out, throttle_out, desired_lateral);
+    gcs().send_text(MAV_SEVERITY_CRITICAL, "steering: %f, throttle %f", steering, speed);
 
     // copy RC scaled inputs to outputs
-    g2.motors.set_throttle(throttle_out);
-    g2.motors.set_steering(steering_out, false);
-    //g2.motors.set_lateral(desired_lateral);
+    g2.motors.set_throttle(speed);
+    g2.motors.set_steering(steering, false);
 }
